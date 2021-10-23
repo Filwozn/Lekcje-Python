c97f4b1486b91a6805bc529c39cefe27d6001f8e
Filwozn <fil.wozniak@gmail.com>
2021-10-24 00:55:38 +0200

    Commit1

--- Lekcja1/Imie_wiek.py
+++ Lekcja1/Imie_wiek.py
@@ -1,3 +1,3 @@
 name = (input("Jak masz na imię? "))
 age = int(input("Ile masz lat? "))
-print("Twoje imię to: ", name, "Masz ", age, "lat.")
+print("Twoje imię to: ", name, "Masz ", age, "lat/a.")
